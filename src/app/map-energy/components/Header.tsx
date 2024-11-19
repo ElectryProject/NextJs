411d8a9acaf5f6a3d473eaf2aeaf4757edaca36e
gabisreis7 <gabisousareis07@gmail.com>
2024-11-19 10:26:05 -0300

    Desenvolvendo página MapEnergy e aplicando a API Google Maps

---
+++ src/app/map-energy/components/Header.tsx
@@ -0,0 +1,29 @@
+"use client"
+import Image from "next/image";
+import { HeaderContainer, NavLinks, PointsButton } from "../../guia-energetico/components/styles";
+import logo from '../../../images/logo (2).png'
+import moeda from '../../../images/moedas.png'
+import Link from "next/link";
+
+
+
+
+
+
+export default function Header(){
+    return(
+        <HeaderContainer>
+            <Image src={logo} alt='Logo Electry' height={30}/>
+            <NavLinks>
+                <li><Link href={'/map-energy'}>MapEnergy</Link></li>
+                <li><Link href={'/token-credit'}>Token Credit</Link></li>
+                <li><Link href={'/loja-energia'}>Loja de Energia</Link></li>
+                <li><Link href={'/integrantes'}>Sobre Nós</Link></li>
+            </NavLinks>
+            <PointsButton>
+                10 pontos
+                <Image src={moeda} alt='Ícone de moedas' height={30}/>
+            </PointsButton>
+        </HeaderContainer>      
+    )
+}
---
+++ src/app/map-energy/components/MapsGoogle.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function MapsGoogle() {
+  return (
+    <div className="mt-9 flex flex-col gap-10">
+      <h1 className="text-center font-bold text-2xl">Acompanhe os <span className="text-[#E5B43B]">Pontos de Coleta no Mapa!</span></h1>
+      <iframe
+        src="https://www.google.com/maps/d/embed?mid=1-QeZrfWszfcdkIPXENnI1YsaqHGHGKU&ehbc=2E312F"
+        width="100%"
+        height="600"
+        style={{ border: "none" }}
+        loading="lazy"
+        title="Meu Mapa do Google My Maps"
+      ></iframe>
+    </div>
+  );
+}
