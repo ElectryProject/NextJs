8365b0e56aa437ce8a09acb5f6469fc5cb434eff
gabisreis7 <gabisousareis07@gmail.com>
2024-11-19 14:32:18 -0300

    Criação da página de Token Credit e aplicação da API Python

--- src/app/map-energy/components/MapsGoogle.tsx
+++ src/app/map-energy/components/MapsGoogle.tsx
@@ -3,7 +3,7 @@
 export default function MapsGoogle() {
   return (
     <div className="mt-9 flex flex-col gap-10">
-      <h1 className="text-center font-bold text-2xl">Acompanhe os <span className="text-[#E5B43B]">Pontos de Coleta no Mapa!</span></h1>
+      <h1 className="text-center font-bold text-2xl">Acompanhe os <span className="text-[#E5B43B]">Pontos de Coleta no Mapa</span>!</h1>
       <iframe
         src="https://www.google.com/maps/d/embed?mid=1-QeZrfWszfcdkIPXENnI1YsaqHGHGKU&ehbc=2E312F"
         width="100%"
