e069a0370d8a7a1aebe2dde22f9746202d884926
gabisreis7 <gabisousareis07@gmail.com>
2024-11-19 23:30:13 -0300

    Aplicação validação em banner para a seção de dúvidas da página Home

--- src/app/components/SectionDuvidas.tsx
+++ src/app/components/SectionDuvidas.tsx
@@ -11,7 +11,8 @@ export default function SectionDuvidas() {
   const [email, setEmail] = useState("");
   const [assunto, setAssunto] = useState("");
   const [mensagem, setMensagem] = useState("");
-  
+  const [successMessage, setSuccessMessage] = useState(""); // Mensagem de sucesso
+  const [errorMessage, setErrorMessage] = useState(""); // Mensagem de erro
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -33,16 +34,35 @@ export default function SectionDuvidas() {
       });
 
       if (response.ok) {
-        alert("Dúvida cadastrada com sucesso!"); // Define a mensagem de sucesso
+        setSuccessMessage("Dúvida cadastrada com sucesso!");
+        setErrorMessage(""); // Limpa a mensagem de erro
         setNome("");
         setEmail("");
         setAssunto("");
         setMensagem("");
+
+        // Remove a mensagem de sucesso após 3 segundos
+        setTimeout(() => {
+          setSuccessMessage("");
+        }, 3000);
       } else {
-        alert("Erro ao enviar a Dúvida, tente novamente!")
+        setErrorMessage("Erro ao enviar a dúvida. Tente novamente!");
+        setSuccessMessage(""); // Limpa a mensagem de sucesso
+
+        // Remove a mensagem de erro após 3 segundos
+        setTimeout(() => {
+          setErrorMessage("");
+        }, 3000);
       }
     } catch (error) {
       console.error('Erro ao conectar com o servidor: ', error);
+      setErrorMessage("Erro ao conectar com o servidor.");
+      setSuccessMessage(""); // Limpa a mensagem de sucesso
+
+      // Remove a mensagem de erro após 3 segundos
+      setTimeout(() => {
+        setErrorMessage("");
+      }, 3000);
     }
   };
 
@@ -55,8 +75,20 @@ export default function SectionDuvidas() {
         <Button><Image src={iconOportunidade} alt='ícone de oportunidade' width={25} />Oportunidades Internas</Button>
       </LeftPanel>
 
-      
       <div className="flex flex-col items-center">
+        {/* Banner de mensagens */}
+        {successMessage && (
+          <div className="p-3 mb-4 w-[700px] text-center text-white bg-green-500 rounded-md">
+            {successMessage}
+          </div>
+        )}
+        {errorMessage && (
+          <div className="p-3 mb-4 w-[700px] text-center text-white bg-red-500 rounded-md">
+            {errorMessage}
+          </div>
+        )}
+
+        {/* Formulário */}
         <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-[700px] mt-20">
           <input
             className="p-3 text-base w-full border border-gray-300 rounded-md"
